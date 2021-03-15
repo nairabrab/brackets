3d028ab950eed6e597ec2ec560c51d373cc86e3c
nairabrab <dan.kobarov@gmail.com>
2021-03-15 23:44:19 +0300

    feat: add 80% valid solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let flag = true;
+  bracketsConfig.forEach(arr => {
+    let i = str.indexOf(arr[0]);
+    let j = str.indexOf(arr[1], i + 1);
+    let k = 0; console.log(i, j);
+    while (flag && k < 100) {
+      console.log(i, j);
+      if (((j - i) % 2) == 0) { flag = false };
+      i = str.indexOf(arr[0], j + 1);
+      if (i < 0) { break; };
+      j = str.indexOf(arr[1], i + 1);
+      if (i > 0 && j < 0) { flag = false };
+      k++;
+    }
+  });
+  if (!flag) { return false };
+  return true;
 }
